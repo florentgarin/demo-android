350e99631d939c2b913d21709fe73634a9840345
Florent Garin <florent.garin@docdoku.com>
2021-12-14 14:59:06 +0100

    chrono complete

--- app/src/main/java/com/training/firstapp/ui/ChronoActivity.java
+++ app/src/main/java/com/training/firstapp/ui/ChronoActivity.java
@@ -9,38 +9,63 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.Locale;
+
 public class ChronoActivity extends AppCompatActivity {
 
     private Button resumeBtn;
-    private Button pauseBtn;
     private Button resetBtn;
     private TextView chronometer;
+
+    private Thread worker;
+    private double sum;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chrono);
-        pauseBtn = findViewById(R.id.pause);
         resumeBtn = findViewById(R.id.resume);
         resetBtn = findViewById(R.id.reset);
         chronometer = findViewById(R.id.chronometer);
 
-        var worker = new Thread(() -> {
-            long startT = System.currentTimeMillis();
-            while(true) {
-                try {
-                    Thread.sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                long currentT = System.currentTimeMillis();
-                Log.i("ChronoActivity", (currentT-startT) + "");
+
+        resumeBtn.setOnClickListener(btn -> {
+            if(worker == null || !worker.isAlive()) {
+                startChrono();
+                resumeBtn.setText("||");
+            }
+            else {
+                worker.interrupt();
+                resumeBtn.setText(">");
             }
         });
 
-        resumeBtn.setOnClickListener(btn -> {
-            worker.start();
+        resetBtn.setOnClickListener(b -> {
+            worker.interrupt();
+            sum = 0;
+            chronometer.setText(String.format(Locale.ENGLISH, "%.2f", sum));
         });
 
     }
+
+    private void startChrono() {
+        worker = new Thread(() -> {
+            double startT = System.currentTimeMillis();
+            double currentT = startT;
+            while(!Thread.interrupted()) {
+                try {
+                    Thread.sleep(50);
+                } catch (InterruptedException e) {
+                    break;
+                }
+                currentT = System.currentTimeMillis();
+                double chronoValue = ((sum + currentT - startT)/1000);
+                runOnUiThread( () -> chronometer.setText(String.format(Locale.ENGLISH, "%.2f", chronoValue)) );
+            }
+            sum += currentT - startT;
+        });
+        worker.start();
+    }
+
 }
